276d339df33236ad88e6bf30ccbefbb41484f0b7
Keith Williams <keith@webizly.com>
2020-01-07 15:44:44 +0200

    Feature: Add and Divide Integers with Tests

--- BasicCalculator/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace BasicCalculator
-{
-    public class Class1
-    {
-    }
-}
---
+++ BasicCalculatorTests/CalculatorTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BasicCalculator;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCalculator.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            Calculator calculator = new Calculator();
+
+            int c = calculator.Add(1, 2);
+
+            Assert.AreEqual(3, c);
+            Assert.AreEqual(3, calculator.result);
+
+        }
+        [TestMethod()]
+        public void DivideTest()
+        {
+            Calculator calculator = new Calculator();
+
+            int c = calculator.Divide(2, 1);
+
+            Assert.AreEqual(2, c);
+            Assert.AreEqual(2, calculator.result);
+
+        }
+    }
+}
